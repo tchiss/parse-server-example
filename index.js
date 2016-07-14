4a5e1547072b6f0e33bbef87b617156f8ee3cffd
tchiss <alainwilfrid@yahoo.com>
2016-07-14 08:22:24 +0200

    index

--- index.js
+++ index.js
@@ -4,17 +4,17 @@
 var express = require('express');
 var ParseServer = require('parse-server').ParseServer;
 
-var databaseUri = process.env.DATABASE_URI || process.env.MONGOLAB_URI
+var databaseUri = 'mongodb://oges_db:>.>vxjNb@ds011442.mlab.com:11442/dev_oges_db'
 
 if (!databaseUri) {
   console.log('DATABASE_URI not specified, falling back to localhost.');
 }
 
 var api = new ParseServer({
-  databaseURI: databaseUri || 'mongodb://tchiss_a:>.>vxjNb@ds055935.mongolab.com:55935/heroku_113wt723',
+  databaseURI: databaseUri || 'mongodb://localhost:27017/dev_oges_db',
   cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
-  appId: process.env.APP_ID || 'KB0XBMX06SVCiUnSUKKgA52v2pee75nSGexrh0wT',
-  masterKey: process.env.MASTER_KEY || '0cLzD2VR4L9Q3iduvkCt2j4YAoUAk3Aj1lEr7hge' //Add your master key here. Keep it secret!
+  appId: process.env.APP_ID || 'ogesApp',
+  masterKey: process.env.MASTER_KEY || 'master' //Add your master key here. Keep it secret!
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
